3ce40b59a2af506243b3524839e022e1851065d4
uthmanduro <udurosinlohun@gmail.com>
2024-07-08 21:30:28 +0100

    modified auth route

--- config.js
+++ config.js
@@ -18,6 +18,18 @@ const sequelize = new Sequelize(process.env.DB_URL) // Example for postgres
 //   logging: console.log
 // });
 
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   port: 5432,
+//   dialect: 'postgres',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+// });
+
+
 const authSync = async () => {
   try {
     await sequelize.authenticate();
